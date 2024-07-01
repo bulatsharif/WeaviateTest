a66e325ccf138dd80eb5ecc4fa00b8e5634a33fb
bulatsharif <b.sharipov@innopolis.university>
2024-07-01 12:01:49 +0300

    environment fix x3

--- src/weaviate_client.py
+++ src/weaviate_client.py
@@ -1,9 +1,8 @@
+from dotenv import load_dotenv
 import weaviate
 import os
 
-os.environ["JINA_AI_API_KEY"] = "your_jina_api_key"
-os.environ["MISTRAL_AI_API_KEY"] = "your_mistral_api_key"
-os.environ["HOST"] = "your_host"
+load_dotenv('.env')
 
 jinaApi: str = os.getenv("JINA_AI_API_KEY")
 mistralApi: str = os.getenv("MISTRAL_AI_API_KEY")

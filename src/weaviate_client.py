bf03d1b729de990231580cbf55d7567f786de5df
bulatsharif <b.sharipov@innopolis.university>
2024-07-01 11:49:27 +0300

    added env testing

--- src/weaviate_client.py
+++ src/weaviate_client.py
@@ -8,10 +8,17 @@ jinaApi: str = os.getenv("JINA_AI_API_KEY")
 mistralApi: str = os.getenv("MISTRAL_AI_API_KEY")
 host: str = os.getenv("HOST")
 
+
+print("--------------------------------------------")
+print(host)
+print(jinaApi)
+print(mistralApi)
+print("--------------------------------------------")
+
 client = weaviate.Client(
-    url="http://158.160.153.243:8080",
+    url=host,
     additional_headers={
-        "X-Jinaai-Api-Key": "jina_5d1f8bfbfcb64374b320054c5627291dy0Ph73OTluT40uUOOVb4vn7cAPAr",
-        "X-Mistral-Api-Key": "RVBRn5Sn26ONsd0CbFBjYWJYR9w416kd"
+        "X-Jinaai-Api-Key": jinaApi,
+        "X-Mistral-Api-Key": mistralApi
     }
 )

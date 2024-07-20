cd90befa425044a61932a7099d9d2298f548d7bc
bulatsharif <b.sharipov@innopolis.university>
2024-07-20 14:03:13 +0300

    Added creation of schema in case of deploy on clear server

--- src/weaviate_client.py
+++ src/weaviate_client.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import weaviate
 import os
 
+from src.schemas import class_article, class_requests, class_saved_news, class_news, class_saved_organization, \
+    class_organization, class_saved_article
+
 load_dotenv('.env')
 
 jinaApi: str = os.getenv("JINA_AI_API_KEY")
@@ -15,3 +18,35 @@ client = weaviate.Client(
         "X-Mistral-Api-Key": mistralApi
     }
 )
+
+if not client.schema.exists("Article"):
+    client.schema.create_class(class_article)
+
+if not client.schema.exists("ArticleSaved"):
+    client.schema.create_class(class_saved_article)
+
+if not client.schema.exists("Organization"):
+    client.schema.create_class(class_organization)
+
+if not client.schema.exists("OrganizationSaved"):
+    client.schema.create_class(class_saved_organization)
+
+if not client.schema.exists("News"):
+    client.schema.create_class(class_news)
+
+if not client.schema.exists("SavedNews"):
+    client.schema.create_class(class_saved_news)
+
+if not client.schema.exists("Request"):
+    client.schema.create_class(class_requests)
+
+
+
+
+
+
+
+
+
+
+
